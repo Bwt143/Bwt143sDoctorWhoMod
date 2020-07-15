6cad30be0c90a399501c48229efecfe55edcc734
Bwt143 <68274795+Bwt143@users.noreply.github.com>
2020-07-15 11:28:52 +0100

    Add files via upload

---
+++ main/java/com/Bwt143/Bwt143sDoctorWhoMod/util/IHasModel.java
@@ -0,0 +1,6 @@
+package com.Bwt143.Bwt143sDoctorWhoMod.util;
+
+public interface IHasModel 
+{	
+	public void registerModels();	
+}
--- main/java/com/Bwt143/Bwt143sDoctorWhoMod/util/Reference.java
+++ main/java/com/Bwt143/Bwt143sDoctorWhoMod/util/Reference.java
@@ -2,7 +2,7 @@ package com.Bwt143.Bwt143sDoctorWhoMod.util;
 
 public class Reference {
 
-	public static final String MOD_ID = "BDWM";
+	public static final String MOD_ID = "bdwm";
 	public static final String NAME = "Bwt143's Doctor Who Mod";
 	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
